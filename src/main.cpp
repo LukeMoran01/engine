6465e1c31813b46fc1d59fdc92df0f9da92f160e
lukemoran01 <2259458m@student.gla.ac.uk>
2024-11-16 18:07:09 +0000

    Revert example change

--- src/main.cpp
+++ src/main.cpp
@@ -874,7 +874,7 @@ class FirstVulkanTriangleApplication {
             }
         }
 
-        VkImageView createImageView(VkImage image, VkFormat format) {
+        VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags) {
             VkImageViewCreateInfo createInfo{};
             createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
             createInfo.image = image;
@@ -890,7 +890,7 @@ class FirstVulkanTriangleApplication {
             createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
 
             // Describes the images purpose and which part we want to access
-            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.aspectMask = aspectFlags;
             createInfo.subresourceRange.baseMipLevel = 0;
             createInfo.subresourceRange.levelCount = 1;
             createInfo.subresourceRange.baseArrayLayer = 0;
