ec761becb41fbdd688008aa5fc4d21457706ba93
lukemoran01 <moran.luke.ed@gmail.com>
2024-11-26 22:56:57 +0000

    Add push constants and starry sky shader

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
 glslc ./shaders/source/vertex/first.vert -o ./shaders/compiled/vert.spv
 glslc ./shaders/source/fragment/first.frag -o ./shaders/compiled/frag.spv
 glslc ./shaders/source/compute/gradient.comp -o ./shaders/compiled/gradient.comp.spv
+glslc ./shaders/source/compute/gradient_color.comp -o ./shaders/compiled/gradient_color.comp.spv
+glslc ./shaders/source/compute/sky.comp -o ./shaders/compiled/sky.comp.spv
