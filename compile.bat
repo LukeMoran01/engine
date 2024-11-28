68ee5bb6e12f48bda66003c45f87f0951cb18ec6
lukemoran01 <moran.luke.ed@gmail.com>
2024-11-28 21:53:27 +0000

    Add basic shaders to create full screen gradient triangle

--- compile.bat
+++ compile.bat
@@ -1,5 +1,9 @@
 glslc ./shaders/source/vertex/first.vert -o ./shaders/compiled/vert.spv
+glslc ./shaders/source/vertex/colored_triangle.vert -o ./shaders/compiled/colored_triangle.vert.spv
+
 glslc ./shaders/source/fragment/first.frag -o ./shaders/compiled/frag.spv
+glslc ./shaders/source/fragment/colored_triangle.frag -o ./shaders/compiled/colored_triangle.frag.spv
+
 glslc ./shaders/source/compute/gradient.comp -o ./shaders/compiled/gradient.comp.spv
 glslc ./shaders/source/compute/gradient_color.comp -o ./shaders/compiled/gradient_color.comp.spv
 glslc ./shaders/source/compute/sky.comp -o ./shaders/compiled/sky.comp.spv
